5d9f2035b229b1ba963c67daf76cc3bcec825f40
Harshvishw <harshpv002@gmail.com>
2024-05-29 23:36:20 +0530

    Finished Project

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,49 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "Which was the first purely object oriented programming language developed?",
+            "Which feature of OOP indicates code reusability?",
+            "Who invented OOP?",
+            "In Unix, which system call creates the new process?",
+            "Which of the following is a type of cyber attack?",
+            "Who is the father of computer security?",
+            "What is the value of the postfix expression 6 3 2 4 + – *?",
+            "Which data structure is used for implementing recursion?",
+            "What is 10+26 ?",
+            "Who invented Telephone?",
+            "what is 12*9 ?",
+            "who is the founder of SpaceX?",
+            "In the given options, which is the Example of System Software?"
+    };
+    public static String choices [][]={
+            {"Kotlin" , "SmallTalk" ,"Java" , "C++"},
+            {"Abstraction" , "Polymorphism" , "Encapsulation" , "Inheritance"},
+            {"Andrea Ferro" , "Adele Goldberg" , "Alan Kay" , "Dennis Ritchie"},
+            {"create" , "fork" , "new" , "none of the mentioned"},
+            {"Phishing" , "SQL Injections" , "Password Attack" , "All of the above"},
+            {"August Kerckhoffs" , "Bob Thomas" , "Robert" , "Charles"},
+            {"74" , "-18" , "12" , "40"},
+            {"Stack" , "Queue" , "List" , "Array"},
+            {"32" , "42" , "36" , "38"},
+            {"Graham Bell" , "Einstein" , "Edison" , "None of the above"},
+            {"96" , "84" , "102" , "108"},
+            {"Jeff Bezos" , "Elon Musk" , "Steve Jobs" , "Bill Gates"},
+            {"Windows" , "Linux" , "MacOS" , "All of the above"}
+    };
+    public static String correctAnswers []={
+            "SmallTalk",
+            "Inheritance",
+            "Alan Kay",
+            "fork",
+            "All of the above",
+            "August Kerckhoffs",
+            "-18",
+            "Stack",
+            "36" ,
+            "Graham Bell",
+            "108",
+            "Elon Musk",
+            "All of the above"};
+}
